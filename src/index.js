fdee09edb0b79d82e1abaafb4a69d8a19cefcf0f
Nutty Nkosi <nkosin361@gmail.com>
2020-04-08 20:33:53 +0200

    added dark mode

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import initTilt from './js/tilt';
 import initSr from './js/sr';
 
 import './style/main.scss';
+import Darkmode from 'darkmode-js';
+
+new Darkmode().showWidget();
+const options = {
+  bottom: '64px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
 
 $('a[href^="#"]').on('click', function(event) {
   var target = $(this.getAttribute('href'));
