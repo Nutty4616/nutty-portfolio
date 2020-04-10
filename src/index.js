543ab65b415e549cf32e27ff58ac68d7a6e1bbe6
Nutty Nkosi <nkosin361@gmail.com>
2020-04-10 21:57:28 +0200

    fixed darkmode error and file updates

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const options = {
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
-  label: '🌓', // default: ''
+  saveInCookies: true, // default: true,
+  label: '🌚', // default: ''
   autoMatchOsTheme: true // default: true
 }
 
