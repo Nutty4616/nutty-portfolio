84f4df923c89c6a747d221d6f162349753e32b43
Nutty Nkosi <nkosin361@gmail.com>
2020-04-29 14:13:08 +0200

    contents fixed

--- src/js/sr.js
+++ src/js/sr.js
@@ -1,5 +1,5 @@
 import assignProps from './assignProps';
-export default function() {
+export default function () {
   const defaultProps = {
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
     distance: '30px',
@@ -7,73 +7,66 @@ export default function() {
     desktop: true,
     mobile: true
   };
-  
+
   /* Section Title */
-  ScrollReveal().reveal('.section-title', 
-    assignProps(
-      {
-        delay:300,distance:'0px', 
-        origin:'bottom'
-      }, defaultProps)
+  ScrollReveal().reveal('.section-title',
+    assignProps({
+      delay: 300,
+      distance: '0px',
+      origin: 'bottom'
+    }, defaultProps)
   );
 
   /* Hero Section */
-  ScrollReveal().reveal('.hero-title', 
-    assignProps(
-      { 
-        delay: 500, 
-        origin: window.innerWidth > 768 ? 'left' : 'bottom'
-      }, defaultProps)
+  ScrollReveal().reveal('.hero-title',
+    assignProps({
+      delay: 500,
+      origin: window.innerWidth > 768 ? 'left' : 'bottom'
+    }, defaultProps)
   );
-  
-  ScrollReveal().reveal('.hero-cta', 
-    assignProps(
-      {
-        delay: 1000, 
-        origin: window.innerWidth > 768 ? 'left' : 'bottom'
-      }, defaultProps)
+
+  ScrollReveal().reveal('.hero-cta',
+    assignProps({
+      delay: 1000,
+      origin: window.innerWidth > 768 ? 'left' : 'bottom'
+    }, defaultProps)
   );
 
   /* About Section */
-  ScrollReveal().reveal('.about-wrapper__image', 
-    assignProps(
-      {
-        delay: 600,
-        origin: 'bottom'
-      }, defaultProps)
+  ScrollReveal().reveal('.about-wrapper__image',
+    assignProps({
+      delay: 600,
+      origin: 'bottom'
+    }, defaultProps)
   );
 
-  ScrollReveal().reveal('.about-wrapper__info', 
-    assignProps(
-      {
-        delay: 1000,
-        origin: window.innerWidth > 768 ? 'left' : 'bottom'
-      }, defaultProps)
+  ScrollReveal().reveal('.about-wrapper__info',
+    assignProps({
+      delay: 1000,
+      origin: window.innerWidth > 768 ? 'left' : 'bottom'
+    }, defaultProps)
   );
 
   /* Projects Section */
-  ScrollReveal().reveal('.project-wrapper__text', 
-    assignProps(
-      {
-        delay: 500,
-        origin: window.innerWidth > 768 ? 'left' : 'bottom'
-      }, defaultProps)
+  ScrollReveal().reveal('.project-wrapper__text',
+    assignProps({
+      delay: 500,
+      origin: window.innerWidth > 768 ? 'left' : 'bottom'
+    }, defaultProps)
   );
 
-  ScrollReveal().reveal('.project-wrapper__image', 
-    assignProps(
-      {
-        delay: 1000,
-        origin: window.innerWidth > 768 ? 'right' : 'bottom'
-      }, defaultProps)
+  ScrollReveal().reveal('.project-wrapper__image',
+    assignProps({
+      delay: 1000,
+      origin: window.innerWidth > 768 ? 'right' : 'bottom'
+    }, defaultProps)
   );
 
   /* Contact Section */
-  ScrollReveal().reveal('.contact-wrapper', 
-    assignProps(
-      {
-        delay: 800,
-        origin: 'bottom'
-      }, defaultProps)
+  ScrollReveal().reveal('.contact-wrapper',
+    assignProps({
+      delay: 800,
+      origin: 'bottom'
+    }, defaultProps)
   );
 }
