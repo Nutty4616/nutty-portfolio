cf332232b6e67f8655fa340852c7801e65fe3aec
Nutty Nkosi <nkosin361@gmail.com>
2020-12-04 22:08:56 +0200

    Updated project(password generator)

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-import initTilt from "./js/tilt";
-import initSr from "./js/sr";
+import initTilt from './js/tilt';
+import initSr from './js/sr';
 
-import "./style/main.scss";
-import Darkmode from "darkmode-js";
+import './style/main.scss';
+import Darkmode from 'darkmode-js';
 
 new Darkmode().showWidget();
 const options = {
-  bottom: "64px", // default: '32px'
-  right: "32px", // default: '32px'
-  left: "unset", // default: 'unset'
-  time: "0.5s", // default: '0.3s'
-  mixColor: "#fff", // default: '#fff'
-  backgroundColor: "#fff", // default: '#fff'
-  buttonColorDark: "#100f2c", // default: '#100f2c'
-  buttonColorLight: "#fff", // default: '#fff'
+  bottom: '64px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff', // default: '#fff'
+  buttonColorDark: '#100f2c', // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: true, // default: true,
-  label: "🌚", // default: ''
+  label: '🌘', // default: ''
   autoMatchOsTheme: false, // default: true
 };
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
 
-$('a[href^="#"]').on("click", function (event) {
-  var target = $(this.getAttribute("href"));
+$('a[href^="#"]').on('click', function (event) {
+  var target = $(this.getAttribute('href'));
   if (target.length) {
     event.preventDefault();
-    $("html, body").stop().animate(
+    $('html, body').stop().animate(
       {
         scrollTop: target.offset().top,
       },
