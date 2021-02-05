aea7a657fcc19211b24829c75bc97eb2fc0ed6c4
Nutty Nkosi <nkosin361@gmail.com>
2021-02-05 15:07:02 +0200

    image optimization and content updates

--- webpack.common.js
+++ webpack.common.js
@@ -1,35 +1,57 @@
-const Webpack = require("webpack");
+const Webpack = require('webpack');
+const ImageminPlugin = require('imagemin-webpack');
+
 module.exports = {
   entry: {
-    vendor: "./src/vendor.js",
-    main: "./src/index.js"
+    vendor: './src/vendor.js',
+    main: './src/index.js',
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ['html-loader'],
       },
       {
         test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[hash].[ext]",
-              outputPath: "assets",
-              esModule: false
-            }
-          }
-        ]
-      }
-    ]
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets',
+              esModule: false,
+            },
+          },
+        ],
+      },
+      {
+        loader: ImageminPlugin.loader,
+        options: {
+          bail: false,
+          cache: false,
+          imageminOptions: {
+            plugins: [
+              ['pngquant', { quality: [0.5, 0.5] }],
+              ['mozjpeg', { quality: 50, progressive: true }],
+              ['gifsicle', { interlaced: true, optimizationLevel: 3 }],
+              [
+                'svgo',
+                {
+                  plugins: [{ removeViewBox: false }],
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
   },
   // This config allows to use jQuery $ sign
   plugins: [
     new Webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    })
-  ]
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 };
